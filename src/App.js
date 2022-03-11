45d089d9fa86fc3994459afbd458bc4f41ad7050
Ben Jackson <benjacksonSE@gmail.com>
2022-03-11 10:00:50 -0500

    Added a header to main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import TopBar from "./components/Topbar/TopBar";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <Home />
     </div>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,17 @@
+import Header from '../../components/Header/Header'
+import Posts from '../../components/Posts/Posts'
+import Sidebar from '../../components/Sidebar/Sidebar'
 import './Home.css'
 
 const Home = () => {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+      <div className='home'>
+        <Posts />
+        <Sidebar />
+      </div>
+    </>
   )
 }
 
