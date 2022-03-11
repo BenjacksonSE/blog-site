de7fd531c87930bd217227963738fd529b4cde9b
Ben Jackson <benjacksonSE@gmail.com>
2022-03-11 10:39:20 -0500

    Styled sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import './Sidebar.css'
+import { FaTwitter, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 
 const Sidebar = () => {
   return (
@@ -13,6 +14,26 @@ const Sidebar = () => {
           Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
           amet ex esse.Sunt eu ut nostrud id quis proident.
         </p>
+      </div>
+      <div className='sidebarItem'>
+        <span className='sidebarTitle'>CATEGORIES</span>
+        <ul className='sidebarList'>
+          <li className='sidebarListItem'>Life</li>
+          <li className='sidebarListItem'>Music</li>
+          <li className='sidebarListItem'>Style</li>
+          <li className='sidebarListItem'>Sport</li>
+          <li className='sidebarListItem'>Tech</li>
+          <li className='sidebarListItem'>Movies</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className='sidebarTitle'>FOLLOW ME</span>
+        <div className='sidebarSocials'>
+          <FaTwitter className='sidebarIcon'/>
+          <FaGithub className='sidebarIcon'/>
+          <FaLinkedin className='sidebarIcon'/>
+          <FaInstagram className='sidebarIcon' />
+        </div>
       </div>
     </div>
   )
