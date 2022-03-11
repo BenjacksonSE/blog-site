4e745c304d9aa458080f488b7914a329e6fbc9e9
Ben Jackson <benjacksonSE@gmail.com>
2022-03-11 11:32:57 -0500

    Began work on post page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import TopBar from "./components/Topbar/TopBar";
 import Home from "./pages/Home/Home";
+import Single from "./pages/Single/Single";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      <Home />
+      {/* <Home /> */}
+      <Single />
     </div>
   );
 }
---
+++ src/components/SinglePost/SinglePost.js
@@ -0,0 +1,28 @@
+import './SinglePost.css'
+import {FaRegEdit, FaRegTrashAlt} from 'react-icons/fa'
+
+const SinglePost = () => {
+  return (
+    <div className='singlePost'>
+      <div className='singlePostWrapper'>
+        <img className="singlePostImg" src='https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt=''/>
+        <h1 className='singlePostTitle'>
+          Lorem ipsum et alta doris
+          <div className='singlePostEdit'>
+            <FaRegEdit className='singlePostIcon'/>
+            <FaRegTrashAlt className='singlePostIcon'/>
+          </div>
+        </h1>
+        <div className='singlePostInfo'>
+          <span className='singlePostAuthor'>Author: <b>Ben Jackson</b></span>
+          <span className='singlePostDate'>1 hour ago</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum eros nibh, a pharetra orci congue in. Maecenas elementum diam ac tortor interdum, non sagittis enim iaculis. Curabitur hendrerit dictum accumsan. Pellentesque sodales massa at dolor ornare, sit amet dignissim lorem imperdiet. Phasellus ultrices fermentum tellus. In iaculis enim ac nisl fringilla pretium. Curabitur magna diam, mollis sed vulputate eget, lobortis vitae diam. Quisque at lectus nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque semper a nunc eu finibus. Curabitur imperdiet id lectus eget convallis. Etiam rutrum sed velit a porttitor. In eleifend massa et orci rutrum dapibus. Sed aliquam magna sem, ac maximus nisl varius at. Mauris maximus, massa sed pharetra lacinia, sapien neque accumsan nisi, id tincidunt ante urna a nisl. Suspendisse bibendum nec quam sed interdum. Praesent accumsan justo lectus, ac hendrerit lorem fringilla et. Donec sit amet eleifend felis. Duis iaculis metus diam, vel congue metus iaculis id. Nam tempor lacinia augue, eget bibendum magna lobortis vel. Nullam feugiat lorem dui, eu tempus neque efficitur non. Ut auctor libero velit, ac molestie urna vulputate vitae. Cras non nunc eu mi consequat varius quis non nunc. In placerat, mauris quis pulvinar gravida, massa odio pharetra est, vitae imperdiet nulla ante sed massa. Quisque pharetra mi ornare, ultrices turpis vitae, consectetur est. Integer vulputate arcu quis odio euismod ornare.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SinglePost
