1ee86a9b6258d872c0e196a3938e12fa63503e0e
Ben Jackson <benjacksonSE@gmail.com>
2022-03-16 10:28:30 -0400

    Added bones to settings page

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -1,9 +1,37 @@
+import Sidebar from '../../components/Sidebar/Sidebar'
 import './Settings.css'
+import {FaRegUserCircle} from 'react-icons/fa'
 
 const Settings = () => {
   return (
     <div className='settings'>
-      Settings page
+      <div className='settingsWrapper'>
+        <div className='settingsTitle'>
+          <span className='settingsUpdateTitle'>Update Your Account</span>
+          <span className='settingsDeleteTitle'>Delete Your Account</span>
+        </div>
+        <form className='settingsForm'>
+          <label>Profile Picture</label>
+          <div className='settingsProfilePic'>
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <FaRegUserCircle className='settingsProfilePicIcon'/>
+            </label>
+            <input type='file' id='fileInput' style={{display: "none"}}/>
+          </div>
+          <label>Username</label>
+            <input type='text' placeholder='Username'/>
+          <label>Email</label>
+            <input type='text' placeholder='Email'/>
+          <label>Password</label>
+            <input type='password'/>
+          <button className='settingsSubmit'>Update Profile</button>
+        </form>
+      </div>
+      <Sidebar />
     </div>
   )
 }
