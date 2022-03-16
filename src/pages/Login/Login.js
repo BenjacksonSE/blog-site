169e8f32cb553892c5acfa190ba097ed1c5abdc1
Ben Jackson <benjacksonSE@gmail.com>
2022-03-16 11:15:46 -0400

    Added styling to login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,11 +3,12 @@ import './Login.css'
 const Login = () => {
   return (
     <div className='login'>
+      <span className='loginTitle'>Login</span>
       <form className='loginForm'>
         <label>Email</label>
-          <input typer='text' placeholder='Enter your email...' />
+          <input className='loginInput' type='text' placeholder='Enter your email...' />
         <label>Email</label>
-          <input typer='text' placeholder='Enter your email...' />
+          <input className='loginInput' type='text' placeholder='Enter your email...' />
         <button className='loginButton'>Login</button>
       </form>
       <button className='loginRegisterButton'>Register</button>
