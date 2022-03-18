77b597756543b5c978374d66a6542f9be52acec4
Ben Jackson <benjacksonSE@gmail.com>
2022-03-18 11:19:05 -0400

    Updated users working correctly, hashed passwords

--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const authRoute = require('./routes/auth')
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
 
 dotenv.config();
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL).then(console.log('connected up')).catch((err) => console.log(err));
 
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
 
 app.listen('3000', ()=>{
   console.log("Backend is ago")
--- api/routes/users.js
+++ api/routes/users.js
@@ -13,6 +13,7 @@ router.put('/:id', async (req,res)=>{
       const updatedUser = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       })
+      res.status(200).json(updatedUser);
     } catch(err){
       res.status(500).json(err);
     }
