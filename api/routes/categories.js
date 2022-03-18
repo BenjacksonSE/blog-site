c2a28d5e3d77033ad8139d02984cef81f42cc496
Ben Jackson <benjacksonSE@gmail.com>
2022-03-18 16:31:11 -0400

    Index categories working

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -2,11 +2,22 @@ const router = require("express").Router();
 const Category = require('../models/Category');
 
 
+//Create
 router.post("/", async (req, res) => {
   const newCategory = new Category(req.body);
   try {
     const savedCategory = await newCategory.save();
     res.status(200).json(savedCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get category by ID
+router.get("/", async (req, res) => {
+  try {
+    const categories = await categories.find();
+    res.status(200).json(categories);
   } catch (err) {
     res.status(500).json(err);
   }
