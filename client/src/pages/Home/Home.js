5fd6f28535e1e133ddf17658d0005d7ea035773a
Ben Jackson <benjacksonSE@gmail.com>
2022-03-19 14:17:41 -0400

    Added dynamic rendering based on category or user

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,19 +3,21 @@ import Header from '../../components/Header/Header'
 import Posts from '../../components/Posts/Posts'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import './Home.css';
-import axios from 'axios' 
+import axios from 'axios';
+import {useLocation} from 'react-router'
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const {search} = useLocation();
+  
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts");
-      setPosts(res.data)
-      console.log(res.data)
+      const res = await axios.get("/posts"+search);
+      setPosts(res.data);
     };
     fetchPosts();
-  },[]);
+  },[search]);
   return (
     <>
       <Header />
