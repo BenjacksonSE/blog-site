4257d3777a2041ed25077c6d1f03b3dc9afbee92
Ben Jackson <benjacksonSE@gmail.com>
2022-03-19 13:14:39 -0400

    Added axios and fixed proxy issue

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,9 +1,20 @@
+import {useState, useEffect} from 'react';
 import Header from '../../components/Header/Header'
 import Posts from '../../components/Posts/Posts'
 import Sidebar from '../../components/Sidebar/Sidebar'
-import './Home.css'
+import './Home.css';
+import axios from 'axios' 
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("/posts");
+      console.log(res)
+    };
+    fetchPosts();
+  },[]);
   return (
     <>
       <Header />
