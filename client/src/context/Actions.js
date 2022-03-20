942e17d6225ed01b75c9ec56b2f1f7da17aa34f4
Ben Jackson <benjacksonSE@gmail.com>
2022-03-20 13:45:59 -0400

    Added basic actions to useContext

---
+++ client/src/context/Actions.js
@@ -0,0 +1,12 @@
+export const LoginStart = (userInfo)=>({
+  typer: "LOGIN_START"
+})
+
+export const LoginSuccess = (user)=>({
+  type:"LOGIN_SUCCESS",
+  payload: user,
+})
+
+export const LoginFailure = ()=>({
+  typer:"LOGIN_FAILURE"
+})
