bc39a7bbc1f34781d8244e5ad35054db8cd8ec23
Ben Jackson <benjacksonSE@gmail.com>
2022-03-20 13:53:54 -0400

    Added contextProvider funtionality

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,4 +1,5 @@
 import {createContext, useReducer} from 'react';
+import Reducer from './Reducer';
 
 const INITIAL_STATE = {
   user:null,
@@ -10,5 +11,16 @@ export const Context = createContext();
 
 
 export const ContextProvider = ({children}) => {
-  
+  const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
+
+  return(
+    <ContextProvider value={{
+      user: state.user,
+      isFetching: state.isFetching,
+      error: state.error,
+      dispatch
+    }}>
+      {children}
+    </ContextProvider>
+  )
 }
