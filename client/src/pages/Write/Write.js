180c3bc4c98a504aa489a40e7c0e1328ae700615
Ben Jackson <benjacksonSE@gmail.com>
2022-03-25 17:51:30 -0400

    Added functionality for changing img, not working :(

--- client/src/pages/Write/Write.js
+++ client/src/pages/Write/Write.js
@@ -10,7 +10,7 @@ const Write = () => {
   const [file, setFile] = useState(null);
   const {user} = useContext(Context);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const newPost = {
       username:user.username,
@@ -22,12 +22,26 @@ const Write = () => {
       const filename = Date.now() + file.name;
       data.append('name',filename)
       data.append('file',file)
+      newPost.pic = filename;
+      try{
+        await axios.post('/upload', data)
+      }catch(err){
+
+      }
     }
-    axios.post('/posts')
+    try{
+      const res = await axios.post('/posts', newPost)
+      window.location.replace('/post/' + res.data._id)
+    }catch(err){
+
+    }
+    
   }
   return (
     <div className='write'>
-      <img src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt='' className='writeImage'/>
+      {file &&(
+          <img src={URL.createObjectURL(file)} alt='' className='writeImage'/>
+      )}
       <form className='writeForm' onSubmit={handleSubmit}>
         <div className='writeFormGroup'>
           <label htmlFor='fileInput'>
