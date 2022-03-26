71040cca1a1b3ce4d80c0d91df23668e46955d82
Ben Jackson <benjacksonSE@gmail.com>
2022-03-25 20:42:15 -0400

    Added all functionality to adding new post with image

--- client/src/pages/Write/Write.js
+++ client/src/pages/Write/Write.js
@@ -18,7 +18,7 @@ const Write = () => {
       desc,
     }
     if(file){
-      const data = FormData();
+      const data = new FormData();
       const filename = Date.now() + file.name;
       data.append('name',filename)
       data.append('file',file)
@@ -38,25 +38,43 @@ const Write = () => {
     
   }
   return (
-    <div className='write'>
-      {file &&(
-          <img src={URL.createObjectURL(file)} alt='' className='writeImage'/>
+    <div className="write">
+      {file && (
+        <img className="writeImage" src={URL.createObjectURL(file)} alt="" />
       )}
-      <form className='writeForm' onSubmit={handleSubmit}>
-        <div className='writeFormGroup'>
+      <form className="writeForm" onSubmit={handleSubmit}>
+        <div className="writeFormGroup">
           <label htmlFor='fileInput'>
             <FaPlus className='writeIcon'/>
           </label>
-          <input type='file' id='fileInput' style={{display:'none'}}/>
-          <input type='text' placeholder='Title' className='writeInput' autoFocus={true}/>
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            className="writeInput"
+            autoFocus={true}
+            onChange={e=>setTitle(e.target.value)}
+          />
         </div>
-        <div className='writeFormGroup'>
-          <textarea placeholder='Write post here...' type='text' className='writeInput writeText'></textarea>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText"
+            onChange={e=>setDesc(e.target.value)}
+          ></textarea>
         </div>
-        <button className='writeSubmit' type='submit'>Publish</button>
+        <button className="writeSubmit" type="submit">
+          Publish
+        </button>
       </form>
     </div>
-  )
+  );
 }
 
 export default Write
